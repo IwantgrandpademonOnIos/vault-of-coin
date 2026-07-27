efc9fec6487fc50e6a05afbad2c80f8833690e4b
When the time comes <edevsi28@gmail.com>
2026-07-27 17:04:40 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,32 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include "ClockVaultLayer.hpp"
 
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        auto btn = CCMenuItemSpriteExtra::create(
+            CCSprite::create("clock_vault_face.png"),
+            this,
+            menu_selector(MenuLayer::onClockVault)
+        );
+
+        btn->setPosition({ winSize.width - 40, 40 });
+
+        auto menu = this->getChildByID("bottom-menu");
+        if (menu) menu->addChild(btn);
+
+        return true;
+    }
+
+    void onClockVault(CCObject*) {
+        auto scene = CCScene::create();
+        scene->addChild(ClockVaultLayer::create());
+        CCDirector::sharedDirector()->pushScene(scene);
+    }
+};
