9bb0c0429ad6619149d980071ca212fcddbcb82e
When the time comes <edevsi28@gmail.com>
2026-07-27 17:04:59 +1000

    Update ClockVaultLayer.cpp

--- src/ClockVaultLayer.cpp
+++ src/ClockVaultLayer.cpp
@@ -1,1 +1,40 @@
+#include "ClockVaultLayer.hpp"
+using namespace geode::prelude;
 
+ClockVaultLayer* ClockVaultLayer::create() {
+    auto ret = new ClockVaultLayer();
+    if (ret && ret->init()) {
+        ret->autorelease();
+        return ret;
+    }
+    delete ret;
+    return nullptr;
+}
+
+bool ClockVaultLayer::init() {
+    if (!CCLayer::init()) return false;
+
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+    auto bg = CCSprite::create("clock_vault_bg.png");
+    bg->setPosition(winSize / 2);
+    this->addChild(bg);
+
+    auto title = CCLabelBMFont::create("???", "bigFont.fnt");
+    title->setPosition({ winSize.width / 2, winSize.height - 60 });
+    this->addChild(title);
+
+    auto prompt = CCLabelBMFont::create("TAP TO INSERT A NUMBER", "goldFont.fnt");
+    prompt->setPosition({ winSize.width / 2, winSize.height - 120 });
+    this->addChild(prompt);
+
+    auto inputBox = CCSprite::create("ui/input_box.png");
+    inputBox->setPosition({ winSize.width / 2, winSize.height - 180 });
+    this->addChild(inputBox);
+
+    auto face = CCSprite::create("clock_vault_face.png");
+    face->setPosition({ winSize.width / 2, 80 });
+    this->addChild(face);
+
+    return true;
+}
